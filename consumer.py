2b79f63aff2a100c85bf2da1d883a641444bfee0
zahraibihsova <zibishova20509@ada.edu.az>
2025-08-27 12:37:49 +0400

    files changed 5

--- consumer.py
+++ consumer.py
@@ -3,9 +3,16 @@ import json
 
 consumer = KafkaConsumer(
     "sensor-data",
-    bootstrap_servers=["52.205.188.82:9092"],
+    bootstrap_servers=["52.205.188.82:9092"],  
+    auto_offset_reset='earliest',
     value_deserializer=lambda v: json.loads(v.decode("utf-8"))
 )
 
-for msg in consumer:
-    print("Received:", msg.value)
+try:
+    for msg in consumer:
+        print("Received:", msg.value)
+except KeyboardInterrupt:
+    print("\nStopping consumer...")
+finally:
+    consumer.close()
+    print("Consumer closed.")
--- producer.py
+++ producer.py
@@ -4,15 +4,22 @@ import time
 import random
 
 producer = KafkaProducer(
-    bootstrap_servers=["52.205.188.82:9092"],
+    bootstrap_servers=["52.205.188.82:9092"],  
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
-while True:
-    data = {
-        "sensor_id": random.randint(1, 5),
-        "temperature": round(random.uniform(20, 30), 2)
-    }
-    producer.send("sensor-data", value=data)
-    print("Sent:", data)
-    time.sleep(2)
+try:
+    while True:
+        data = {
+            "sensor_id": random.randint(1, 5),
+            "temperature": round(random.uniform(20, 30), 2)
+        }
+        producer.send("sensor-data", value=data)
+        print("Sent:", data)
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\nStopping producer...")
+finally:
+    producer.flush()  # for being sure all messages are sent
+    producer.close()
+    print("Producer closed")
